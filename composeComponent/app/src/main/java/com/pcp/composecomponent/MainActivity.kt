a95e786ba8b925bc31c4fe6e14dc9808ee115d46
Joey yang <yty0606@gmail.com>
2022-05-03 17:52:55 +0800

    Feat: Join OutlinedTextField, DropdownMenu
    Solution:
    1. As title.  Complete OutlinedTextField and DropdownMenu (this property still need be completed.
    
    Author: Joey

--- composeComponent/app/src/main/java/com/pcp/composecomponent/MainActivity.kt
+++ composeComponent/app/src/main/java/com/pcp/composecomponent/MainActivity.kt
@@ -10,27 +10,40 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pcp.composecomponent.ui.theme.*
+import kotlinx.coroutines.selects.selectUnbiased
 
 /*
   Author: Joey yang
@@ -116,6 +129,74 @@ fun FirstScreen(navController: NavController) {
             )
         }
         ButtonDemo()
+        DropdownMenuDemo()
+    }
+}
+
+@Composable
+fun DropdownMenuDemo() {
+    var expanded by remember { mutableStateOf(false) }
+    val suggestions = listOf("Item1", "Item2", "Item3",)
+    var selectedText by remember { mutableStateOf("") } //重要: 這會要你 import library,最後AS會直接 import androidx.compose.runtime.*
+    var textfieldSize by remember { mutableStateOf(Size.Zero) }
+    val keyboardOption = KeyboardOptions(autoCorrect = true)   //重要,共有四項,都可以再加
+    val keyboardAction = KeyboardActions(onDone = {})
+
+    val icon2 = if (expanded) Icons.Filled.ArrowUpward else Icons.Filled.ArrowDownward
+    val icon = if (expanded) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown
+
+    // 重要,下面三行是 interactionSource 的使用
+    val interactionSourceTest = remember { MutableInteractionSource() }
+    val pressState = interactionSourceTest.collectIsPressedAsState()
+    val borderColor = if (pressState.value) YellowFFEB3B else Green4CAF50 //Import com.pcp.composecomponent.ui.theme.YellowFFEB3B
+
+    OutlinedTextField(
+        value = selectedText,
+        onValueChange = { selectedText = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned { coordinates ->  //重要
+                textfieldSize = coordinates.size.toSize()
+            },
+        enabled = true,
+        readOnly = true,
+        textStyle = TextStyle(color = Color.Blue, fontWeight = FontWeight.Bold),
+        label = { Text("Label")},
+        placeholder = { Text("Enter info") },
+        leadingIcon = {    //重要
+            Icon(icon2, "contentDescription",
+                Modifier.clickable { expanded = !expanded })
+        },
+        trailingIcon = {    //重要
+            Icon(icon, "contentDescription",
+                Modifier.clickable { expanded = !expanded })
+        },
+        isError = false,    //指示是否text fields的目前值是有錯的,若true, label, bottom indicator和 trailingIcon 預設都顯示錯誤的顏色
+        visualTransformation = PasswordVisualTransformation(), //可看原始碼
+        keyboardOptions = keyboardOption,
+        keyboardActions = keyboardAction,
+        singleLine = false,
+        maxLines = 2,
+        interactionSource = interactionSourceTest,
+        shape = RoundedCornerShape(8.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            backgroundColor = YellowFFEB3B),
+    )
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        modifier = Modifier
+            .width(with(LocalDensity.current) {
+                textfieldSize.width.toDp() })
+    ) {
+        suggestions.forEach { label ->
+            DropdownMenuItem(onClick = {
+                selectedText = label
+                expanded = false
+            }) {
+                Text(text = label)
+            }
+        }
     }
 }
 
